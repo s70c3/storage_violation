a49f5bcf13f99fd08dabea3186d9e08e53bdc59c
s70c3 <mari.s70c3@gmail.com>
2026-03-20 12:59:01 +0100

    set model half

--- source/api.py
+++ source/api.py
@@ -22,7 +22,7 @@ if not LOGGER.handlers:
 MODEL_WEIGHTS_PATH = "weights/cd_weights.pt"
 MODEL_DEVICE = "cuda:0"   # e.g. "cuda:0"
 MODEL_THRESHOLD = 0.5
-MODEL_HALF = False
+MODEL_HALF = True
 MODEL_INP_CH = 9
 
 PROCESSOR_KWARGS = dict(
